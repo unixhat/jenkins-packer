5b98ea390eaec5cb46c794bcc06bb2f6f064dbb6
heriyanto <heriyanto@Changes-MacBook-Pro.local>
2017-07-18 13:02:08 +0700

    remote has deprecated changed with state pull

--- scripts/configure-remote-state.sh
+++ scripts/configure-remote-state.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-terraform remote config -backend=s3 -pull=false -backend-config="bucket=terraform-state-heri1" -backend-config="key=terraform/terraform.tfstate" -backend-config="region=eu-west-1"
+#terraform remote config -backend=s3 -pull=false -backend-config="bucket=terraform-state-heri1" -backend-config="key=terraform/terraform.tfstate" -backend-config="region=eu-west-1"
+terraform state pull
